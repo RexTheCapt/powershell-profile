68916c9a38797d7a45230485d6d9739fcc789eb9
rexthecapt <rexthecapt@gmail.com>
2022-11-01 12:34:07 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,26 @@
 #If the file does not exist, create it.
 if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
-     try {
-         Invoke-RestMethod https://github.com/RexTheCapt/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
-         Write-Host "The profile @ [$PROFILE] has been created."
-     }
-     catch {
-         throw $_.Exception.Message
-     }
- }
+    try {
+        $path = Split-Path $PROFILE -Parent
+        $path = $path+'\'
+
+        if (!(Test-Path $path)) {
+            New-Item -ItemType Directory $path
+        }
+
+        Invoke-RestMethod https://github.com/RexTheCapt/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+        Write-Host "The profile @ [$PROFILE] has been created."
+    }
+    catch {
+       throw $_.Exception.Message
+    }
+}
 # If the file already exists, show the message and do nothing.
- else {
-		 Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
-		 Invoke-RestMethod https://github.com/RexTheCapt/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
-		 Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
- }
+else {
+		Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
+		Invoke-RestMethod https://github.com/RexTheCapt/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+		Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
+}
 & $profile
 
 # OMP Install
